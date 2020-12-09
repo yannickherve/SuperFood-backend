d11078b97d409c918d381c25af0ad9c631df36b8
yannickherve <dogneyannick@yahoo.fr>
2020-12-09 20:03:02 +0100

    Finish adding environnement variable

--- db/mongoose..js
+++ db/mongoose..js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://127.0.0.1:27017/super-food-api', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
